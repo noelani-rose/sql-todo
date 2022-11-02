c612f4e3491e03a869f06fcba515dbfec8ff68f6
Noelani West <noelani.rose14@gmail.com>
2022-11-02 16:08:41 -0500

    create database url

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,19 @@
 const pg = require('pg');
 
-let pool = new pg.Pool({
-  database: "cloud_todo"
-});
 
+// database url
+// postgres://postgress:password@awsendpoint
+// postgres://postgres:Dgaf14790552126??@ramirez-noelani-demo-db.chi55kmnapwc.us-east-1.rds.amazonaws.com
+
+let pool;
+if (process.env.DATABASE_URL){
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+  })
+}
+else {
+  let pool = new pg.Pool({
+    database: "cloud_todo"
+  });
+}
 module.exports = pool;
