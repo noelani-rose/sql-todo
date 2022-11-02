5c3917ce9d2dfda9b7428bf9b55089e2a0448dd8
Noelani West <noelani.rose14@gmail.com>
2022-11-02 16:21:38 -0500

    ignore env

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -2,8 +2,8 @@ const pg = require('pg');
 
 
 // database url
-// postgres://postgress:password@awsendpoint
-// postgres://postgres:Dgaf14790552126??@ramirez-noelani-demo-db.chi55kmnapwc.us-east-1.rds.amazonaws.com
+// postgres://postgress:password@awsendpoint/dbname
+// postgres://postgres:Dgaf14790552126??@ramirez-noelani-demo-db.chi55kmnapwc.us-east-1.rds.amazonaws.com/cloud-todo
 
 let pool;
 if (process.env.DATABASE_URL){
